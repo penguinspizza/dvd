a95df7dd2c6647bad37124b82ef64b04b37df1ef
penguinspizza <72650078+penguinspizza@users.noreply.github.com>
2021-05-09 16:23:54 +0900

    Add inf

--- js/main.js
+++ js/main.js
@@ -23,6 +23,8 @@ class Dvds {
 
 dvd_list = []
 
+var infinity = false;
+
 function add_dvd() {
     dvd_list.push(new Dvds(
         getRandomInt(60, canvas.width - 60),
@@ -31,7 +33,15 @@ function add_dvd() {
         2 * getRandomInt(0, 2) - 1,
         getRandomColor()
         ));
-    
+}
+
+function inf() {
+    infinity = true;
+}
+
+function res() {
+    dvd_list.splice(1);
+    infinity = false;
 }
 
 add_dvd();
@@ -57,10 +67,16 @@ function draw() {
         if (element.x + element.dx > canvas.width - text_info.actualBoundingBoxLeft|| element.x + element.dx < text_info.actualBoundingBoxRight)  { // もしx軸上の範囲外に行ったら
             element.dx = -element.dx; // dxの正負を逆転
             element.color = getRandomColor();
+            if (infinity === true) {
+                add_dvd();
+            }
         }
         if (element.y + element.dy > canvas.height - text_info.actualBoundingBoxDescent || element.y + element.dy < text_info.actualBoundingBoxAscent) { // もしy軸上の範囲外に行ったら
             element.dy = -element.dy; // dyの正負を逆転
             element.color = getRandomColor();
+            if (infinity === true) {
+                add_dvd();
+            }
         }
         element.x += element.dx;
         element.y += element.dy;
